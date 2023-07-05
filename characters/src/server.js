cc6e2cd05144b922c63bb38de7a88b9dad02b3d1
Edgar <davidnavarro81230@outlook.com>
2023-07-04 20:44:09 -0500

    database microservice

--- characters/src/server.js
+++ characters/src/server.js
@@ -7,6 +7,17 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 
-server.use(router);
+server.use("/characters", router);
+
+server.use("*", (req, res) => {
+    res.status(404).send("Not found");
+});
+
+server.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+        error: true,
+        message: err.message
+    })
+})
 
 module.exports = server;
--- films/src/server.js
+++ films/src/server.js
@@ -7,6 +7,10 @@ const server = express();
 server.use(morgan("dev"));
 server.use(express.json());
 
-server.use(router);
+server.use("./films", router);
+
+server.use("*", (req, res) => {
+    res.status(404).send("Not found");
+});
 
 module.exports = server;
