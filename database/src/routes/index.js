d91d8490b92049ae94aeebb6dfcce64b9ec5d676
Edgar <davidnavarro81230@outlook.com>
2023-07-06 15:59:35 -0500

    middlewares

---
+++ database/src/routes/index.js
@@ -0,0 +1,13 @@
+const {Router} = require("express");
+const store = require("../database");
+const {validateModel} = require("../middlewares");
+
+const router = Router();
+
+router.get("/:model", validateModel, async (req, res) => {
+    const {model} = req.params;
+    const response = await store[model].list();
+    res.status(200).json(response);
+});
+
+module.exports = router;
