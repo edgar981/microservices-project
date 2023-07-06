f16ec83be66d138c5ddd62272ddafc1a1b5f8a0d
Edgar <davidnavarro81230@outlook.com>
2023-07-05 19:41:43 -0500

    static models

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -3,8 +3,6 @@ const {MONGODB_URI} = require("../config/envs")
 
 const connection = mongoose.createConnection(MONGODB_URI);
 
-Character.find().populate("films", ["_id", "title"]).then(res => console.log(res[0]));
-
 module.exports = {
     Character: connection.model("Character", require("./schemas/characterSchema")),
     Film: connection.model("Film", require("./schemas/filmSchema")),
