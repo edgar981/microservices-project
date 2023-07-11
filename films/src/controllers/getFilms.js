b5d7e3161095bee63ee57087d1e3414dcf57f0cf
Edgar <davidnavarro81230@outlook.com>
2023-07-11 13:09:01 -0500

    Fixing Planet and Film model

--- films/src/controllers/getFilms.js
+++ films/src/controllers/getFilms.js
@@ -1,7 +1,7 @@
-const Films = require("../data");
+const Film = require("../data");
 const {response} = require("../utils");
 
 module.exports = async (req, res) => {
-    const films = await Films.list();
+    const films = await Film.list();
     response(res, 200, films);
 }
